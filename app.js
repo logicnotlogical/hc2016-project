c7db455d27120661a0769bd43115b760965a8655
chilichilimo <afshar.m.95@gmail.com>
2016-01-30 23:35:39 +0000

    Uploading to lingualmingle.net

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var exphbs = require('express-handlebars');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var login = require('./routes/login');
+var signup = require('./routes/signup');
 
 var app = express();
 
@@ -29,6 +30,7 @@ app.use('/bower_components', express.static(path.join(__dirname, 'bower_componen
 app.use('/', routes);
 app.use('/users', users);
 app.use('/login',login);
+app.use('/signup',signup);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
