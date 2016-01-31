9085000735198b8c7e9b5daaf8695a5eda107bc8
Ashu Savani <ashusavani@Ashus-MacBook-Pro-7.local>
2016-01-31 06:22:45 +0000

    when you don't know how to implement bcrypt :'(

--- db.js
+++ db.js
@@ -6,7 +6,15 @@ var check;
 
 function login(username, password)
 {
-    
+    var bcrypt = require('bcrypt'); //function for hashing the password
+    bcrypt.genSalt = (10, function(err, salt)
+    {
+    	bcrypt.hash = (password, salt, function(err, hash)
+    	{
+    		password = hash;
+    	});
+    });
+    db.run()
 }
 
 module.exports = {
