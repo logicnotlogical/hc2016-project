e0fb13e95b1cf0c5c47ccaf9f5693599155ed335
Matthew Bell <logicnotlogical@outlook.com>
2016-01-31 08:26:51 +0000

    wha

--- db.js
+++ db.js
@@ -2,24 +2,28 @@
 
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('lingualmingle.sqlite');
+var bcrypt = require('bcrypt'); //function for hashing the password
 var check;
 
 function login(username, password)
 {
-    var bcrypt = require('bcrypt'); //function for hashing the password
     bcrypt.genSalt = (10, function(err, salt)
-    {
-    	bcrypt.hash = (password, salt, function(err, hash)
-    	{
-    		password = hash;
-    	});
-    });
+            {
+                bcrypt.hash = (password, salt, function(err, hash)
+                        {
+                            password = hash;
+                        });
+            });
     db.run("INSERT INTO lingualmingle(col1, col2) VALUES (password, username)"); //not sure if this is correct(it's meant to be the insert function)
 }
 
 function signup(user, callback) {
-    if (user.username != null && user.password != null && user.email != null) {
-        
+    // check that the required fields are present.
+    if (user.username != null && user.password != null && user.email != null && user.firstName != null) {
+        var language = user.language : "en"; // defaults to english.
+        bcrypt.genSalt = (10, function(err, salt) {
+            bcrypt.hash(user.password, 8, function(err, hash) {
+                db.run("INSERT INTO users (Email, FirstName, 
     }
     else callback(false);
 };
